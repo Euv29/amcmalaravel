8cf7fdc7198099c581642fb29ea48c5ece429b96
Venâncio Wapinda <euvenancio1@gmail.com>
2024-02-10 22:22:55 +0100

    Update whith tailwindCss

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+  // Seu ponto de entrada principal
+  // Você pode ajustar este caminho de acordo com a estrutura do seu projeto
+  // Por exemplo, se seu arquivo principal estiver em 'resources/js/app.js', você pode usar 'resources/js/app.js' aqui
+  root: './resources/js',
+
+  // Arquivos a serem pré-bundled, se necessário
+  optimizeDeps: {
+    include: [
+      'vue', // Exemplo: incluir Vue.js
+      '@tailwindcss/forms', // Exemplo: incluir @tailwindcss/forms
+      // Adicione outros módulos conforme necessário
     ],
+  },
 });
